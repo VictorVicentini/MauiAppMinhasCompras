8122d769c721b5130a27d72698aaf84720f00998
victor vicentini <victorvicentini@yahoo.com.br>
2026-03-06 10:11:16 -0300

    add Models e alteração da tela inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -4,6 +4,8 @@ namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+
+
         public App()
         {
             InitializeComponent();
@@ -11,7 +13,8 @@ namespace MauiAppMinhasCompras
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            // return new Window(new AppShell());
+            return new Window(new NavigationPage(new Views.ListaProduto()));
         }
     }
 }
